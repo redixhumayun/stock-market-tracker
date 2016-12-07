71fa718d23d613c814533297c698821960d25f76
Zaid Humayun <redixhumayun@gmail.com>
2016-12-08 01:58:49 +0530

    Just added input validation. Still need to add charts

--- stock-client/src/containers/Search.js
+++ stock-client/src/containers/Search.js
@@ -7,29 +7,51 @@ class Search extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			value: ''
+			value: '', 
+			invalid: false
 		}
 		this.handleChange = this.handleChange.bind(this);
+		this.inputValidation = this.inputValidation.bind(this);
 	}
 
 	handleChange(event){
 		this.setState({value: event.target.value.toUpperCase()})
 	}
 
+	inputValidation(value){
+		return new Promise((resolve, reject) => {
+			if(value.match(/^[a-zA-Z]+$/)){
+				resolve()
+			}else{
+				reject()
+			}
+		})
+	}
+
 	render() {
 		return (
-			<div className='input-group'>
-				<input ref='input' type='text' className='form-control' placeholder='Enter stock ticker' value={this.state.value}
-				onChange={this.handleChange}/>
-				<span className='input-group-btn'>
-					<button className='btn btn-primary' type='submit' 
-					onClick={() => {
-						this.props.onAddTicker(this.props.currentState, this.state.value)
-						this.state.value = ''
-					}}>
-						Add
-					</button>
-				</span>
+			<div>
+				<div className='input-group'>
+					<input ref='input' type='text' className='form-control' placeholder='Enter stock ticker' value={this.state.value}
+					onChange={this.handleChange}/>
+					<span className='input-group-btn'>
+						<button className='btn btn-primary' type='submit' 
+						onClick={() => {
+							this.inputValidation(this.state.value).then(() => {
+								this.props.onAddTicker(this.props.currentState, this.state.value)
+								this.state.value = ''
+							}, (err) => {
+								this.setState({invalid: true})
+								console.log(this.state);
+							})
+						}}>
+							Add
+						</button>
+					</span>
+				</div>
+				<div>
+					{this.state.invalid ? <p>Invalid entry</p> : null}
+				</div>
 			</div>
 		)
 	}
